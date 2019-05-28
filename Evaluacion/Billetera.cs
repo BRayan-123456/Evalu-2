4a5b810afbbf8785aa62c178eba228d8c3d19c67
BRayan-123456 <41800589+BRayan-123456@users.noreply.github.com>
2019-05-27 22:22:39 -0500

    Initial commit

---
+++ Evaluacion/Billetera.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Evaluacion
+{
+    class Billetera
+    {
+
+        public double valorIngreso { get; set; }
+        public double valorGastado { get; set; }
+
+        public double valortotal { get; set; }
+        public Billetera() { }
+        public double Gastar(double gastar) {
+            valorGastado = gastar;
+            var resultado1 = valorIngreso - gastar;
+            return resultado1;
+        }
+
+       public void ImprimirDinero(double arriba)
+        {
+            valortotal = arriba;
+            Console.WriteLine($"el valor que hay en la cuenta es: {arriba}");
+
+        }
+   
+    }
+
+    
+}
---
+++ Evaluacion/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Evaluacion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Billetera billetera = new Billetera();
+            Console.WriteLine($"Inserte el valor");
+            billetera.valorIngreso = int.Parse(Console.ReadLine());
+
+      
+            Console.WriteLine($"Digite el valor que va a descontar");
+             var valorgasta=(Convert.ToDouble(Console.ReadLine()));
+
+
+            Console.WriteLine($"{billetera.Gastar(valorgasta)}");
+            Console.ReadLine();
+
+            
+
+           double valortotal = billetera.Gastar(valorgasta);
+            billetera.ImprimirDinero(valortotal);
+          
+
+
+
+        }
+}
+}
